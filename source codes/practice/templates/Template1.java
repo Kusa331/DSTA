158cdf91cf70747277c009b4369f9df5de082c27
cbatuic <cbalaman@uic.edu.ph>
2024-06-25 11:15:15 +0800

    Create Template1.java

---
+++ source codes/practice/templates/Template1.java
@@ -0,0 +1,34 @@
+package Challenges;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author clydebalaman
+ */
+public class Template1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int T = sc.nextInt();
+        while(T-- > 0){
+            // Your solution codes here..
+            int num = sc.nextInt();
+            System.out.println(num);
+        }        
+    }
+}
+
+/*
+Sample Input:
+3
+10
+20
+30
+
+Sample Output:
+10
+20
+30
+ 
+*/
